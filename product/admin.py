dd710f4df02201f222e3430df8f369e72fdae837
Nurmuhammad0071 <nurmuhammadsultonovich0571@gmail.com>
2024-02-04 22:15:00 +0500

    feat:Add Admin and fix: Model

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,82 @@
 from django.contrib import admin
+from django.forms import ModelForm
+
+from .models import Category, Size, Color, Product, ProductImage, Price
+
+
+# Inlines
+class CategoryInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = Category
+    extra = 1
+
+
+class SizeInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = Size
+
+
+class ColorInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = Color
+    extra = 1
+
+
+class ColorInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = Color
+    extra = 1
+
 
 # Register your models here.
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
+    inlines = [SizeInline, ColorInline]
+
+    # inlines = []
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
+    # inlines = []
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
+
+    # inlines = []
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'slug', 'text']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['title']
+
+    # inlines = []
+
+
+@admin.register(ProductImage)
+class PrImageAdmin(admin.ModelAdmin):
+    list_display = ['product']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['product']
+
+
+@admin.register(Price)
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ['product']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['count']
+
--- product/models.py
+++ product/models.py
@@ -12,34 +12,54 @@ class BaseModel(models.Model):
 
 class Category(BaseModel):
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Size(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sizes')
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Color(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='colors')
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)  # Bu sana buyicha unique qilingan
     text = models.TextField()
+
+    def __str__(self):
+        return self.title
 
 
 class ProductImage(BaseModel):
     product = models.OneToOneField(Product, on_delete=models.CASCADE, primary_key=True, related_name='image')
     image = models.ImageField(upload_to='product_images/')
 
+    def __str__(self):
+        return f"Image for Product: {self.product.title}"
+
 
 class Price(BaseModel):
     size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='prices_by_size')
     color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name='prices_by_color')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='prices')
+    count = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"Option for Product: {self.product.title}"
