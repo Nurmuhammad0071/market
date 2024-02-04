2a60ecaf9a433bc44d3b29fc152a0fae05b246aa
Nurmuhammad0071 <nurmuhammadsultonovich0571@gmail.com>
2024-02-04 18:09:26 +0500

    feat:Add models

--- product/models.py
+++ product/models.py
@@ -1,42 +1,45 @@
-# from django.db import models
-#
-#
-# class BaseModel(models.Model):
-#     status = models.BooleanField(default=0)
-#     created_at = models.DateTimeField(auto_now=True)
-#     updated_at = models.DateTimeField(auto_now_add=True)
-#
-#
-# class Category(BaseModel):
-#     title = models.CharField(max_length=255)
-#     slug = models.SlugField(max_length=255)
-#
-#
-# class Size(BaseModel):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=255)
-#     slug = models.SlugField(max_length=255)
-#
-#
-# class Color(BaseModel):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=255)
-#     slug = models.SlugField(max_length=255)
-#
-#
-# class Product(BaseModel):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     title = models.CharField(max_length=255)
-#     text = models.TextField()
-#
-#
-# class ProductImage(BaseModel):
-#     product = models.OneToOneField(Product, on_delete=models.CASCADE, primary_key=True)
-#     image = models.ImageField(upload_to='product_images/')
-#
-#
-# class Price(BaseModel):
-#     size = models.ForeignKey(Size, on_delete=models.CASCADE)
-#     color = models.ForeignKey(Color, on_delete=models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-#     price = models.FloatField()
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class Category(BaseModel):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+
+class Size(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='sizes')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+
+class Color(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='colors')
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+
+
+class Product(BaseModel):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+
+class ProductImage(BaseModel):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE, primary_key=True, related_name='image')
+    image = models.ImageField(upload_to='product_images/')
+
+
+class Price(BaseModel):
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='prices_by_size')
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, related_name='prices_by_color')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='prices')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
